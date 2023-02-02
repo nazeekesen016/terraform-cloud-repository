56228c85f0d38a5c67aebc718cf267c18242d6c3
nazeekesen016 <nasik2431@gmail.com>
2023-02-02 09:03:44 +0600

    teraform

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
-provider "aws" {
-  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
 }
-
-
 terraform {
   cloud {
     organization = "terraform-cloud-organizaiton"
 
-    workspaces{
-        name = "common"
+    workspaces {
+      name = "common"
     }
   }
 }
 
-resource "aws_vpc" "vpc" {
-  
-}
+resource "aws_vpc" "vpc" {}
